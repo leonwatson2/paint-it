71143cd0837739616e1b66cb64eb933a098c0ab4
leonwatson2 <leon@vlw2.com>
2024-11-09 21:58:52 -0600

    Added headers and updated css to use Tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import "./App.css";
 import { BrushSvg, BucketSvg } from "./BucketSvg";
 import { Button } from "./components/ui/button";
 import { PaintBoard } from "./PaintBoard";
-import { COLORS, initialImage } from "@/lib/constants";
+import { initialImage } from "@/lib/constants";
 import { useRottingOranges } from "./hooks/useRottingOranges";
 import { cn } from "./lib/utils";
 import { usePaintControls } from "./hooks/usePaintControls";
+import { ColorPalette } from "./components/ColorPalette";
 
 function App() {
   const { setPaintMode, setPaintColor, paintMode, paintColor } = usePaintControls();
@@ -18,18 +19,21 @@ function App() {
 
   return (
     <>
+      <h1 className="text-6xl text-center mb-12">Algo tester</h1>
       <div className="container block">
+        <h2 className="algo-header">994: Rotting Oranges</h2>
         <div className="image">
           <PaintBoard image={orangeImage} onPaint={onOrangePaint} />
         </div>
         <div className="text-6xl flex flex-col justify-center items-center">
-          <h2 onClick={resetOranges} className="text-6xl cursor-pointer">{steps}</h2>
+          <h2 onClick={resetOranges} title="Reset" className="text-6xl cursor-pointer">{steps}</h2>
           <div className="space-y-4 flex items-center justify-center flex-col">
-            <Button onClick={onRun} >Run the thing </Button>
+            <Button onClick={onRun}>Run the thing </Button>
           </div>
         </div>
       </div>
       <div className="container">
+        <h2 className="algo-header">Painting (BFS)</h2>
         <div className="image">
           <PaintBoard image={image} onPaint={onPaint} />
         </div>
